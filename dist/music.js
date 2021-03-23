18216647a61bf24e8c791035655d8e53ebd50c77
yaodaobaiguiwan <1714594511@qq.com>
2021-03-23 10:36:35 +0800

    Site updated: 2021-03-23 10:36:34

--- dist/music.js
+++ dist/music.js
@@ -6,31 +6,25 @@ const ap = new APlayer({
 	{
 	name: "双人舞 第二幕第三场第14首",
 	artist: 'Pyotr Ilyich Tchaikovsky',
-	url: 'http://m10.music.126.net/20210323085746/0ecfc3ec2484b5f929f13ddfec786373/ymusic/ccae/888f/4f48/8c67340af1d69e12e15b0c38f677eeca.mp3',
+	url: 'http://103.224.249.63/双人舞 第二幕第三场第14首.mp3',
 	},
 
 	{
 	name: "莫扎特 - 小步舞曲",
         artist: 'Wolfgang Amadeus Mozart',
-        url: 'http://m10.music.126.net/20210323103053/758c467fa5854351746d31b66d80ed91/ymusic/380d/2179/e4c8/048ad6b300de17413689d22136b53c9a.mp3',
-      },
-
-	{
-        name: "太阳照常升起",
-        artist: '久石让',
-        url: 'http://m10.music.126.net/20210323091549/d511ffe964e5832c397884789b08765f/ymusic/5614/195d/ad51/33fff191fffc2fd5da6c94d71e7777ef.mp3',
+        url: 'http://103.224.249.63/莫扎特 - 小步舞曲.mp3',
       },
 
 	{
         name: "原来",
         artist: '林俊杰',        
-	url: 'http://m10.music.126.net/20210323102609/313588081a313f4cd43683c0c5bb7463/ymusic/obj/w5zDlMODwrDDiGjCn8Ky/3094661786/6904/0472/3c23/bbbedff50edf5fcb8b2eb02f731f3e48.mp3',
+	url: 'http://103.224.249.63/原来.mp3',
       },
 
 	{
         name: "倒带",
         artist: '蔡依林',
-        url: 'http://m10.music.126.net/20210323092554/f73cc7ec59d5b35ea5516a4d1aa5b2af/ymusic/0c70/b478/049f/e602701f487136ebb36d26581e4e6aad.mp3',
+        url: 'http://103.224.249.63/倒带.mp3',
       },
 
 	
