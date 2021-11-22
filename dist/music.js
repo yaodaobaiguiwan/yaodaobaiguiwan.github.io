3bef36bcf32065de37adf9b13c842fad5cbac971
yaodaobaiguiwan <1714594511@qq.com>
2021-11-22 10:21:54 +0800

    Site updated: 2021-11-22 10:21:54

--- dist/music.js
+++ dist/music.js
@@ -6,30 +6,30 @@ const ap = new APlayer({
 	{
 	name: "双人舞 第二幕第三场第14首",
 	artist: 'Pyotr Ilyich Tchaikovsky',
-	url: 'http://103.224.249.63/双人舞 第二幕第三场第14首.mp3',
+	url: 'http://xz.itcytblog.cn/双人舞 第二幕第三场第14首.mp3',
 	},
 
 	{
 	name: "莫扎特 - 小步舞曲",
         artist: 'Wolfgang Amadeus Mozart',
-        url: 'http://103.224.249.63/莫扎特 - 小步舞曲.mp3',
+        url: 'http://xz.itcytblog.cn/莫扎特 - 小步舞曲.mp3',
       },
 
 	{
         name: "原来",
         artist: '林俊杰',        
-	url: 'http://103.224.249.63/原来.mp3',
+	url: 'http://xz.itcytblog.cn/原来.mp3',
       },
 
 	{
         name: "倒带",
         artist: '蔡依林',
-        url: 'http://103.224.249.63/倒带.mp3',
+        url: 'http://xz.itcytblog.cn/倒带.mp3',
       },
 	{
 	name: "世界末日",
         artist: '周杰伦',
-        url: 'http://103.224.249.63/周杰伦 - 世界末日.mp3',
+        url: 'http://xz.itcytblog.cn/周杰伦 - 世界末日.mp3',
       }
 	
     ]
